9e8e42d441cacd0c61dd73314b71a797a423c1a9
user <user@DESKTOP-53176P4>
2023-02-16 16:12:18 +0530

    Add project files.

---
+++ FisrtMVCProject/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using FisrtMVCProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FisrtMVCProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Student student = new Student();
+            student.Name = "Monika";
+            student.City = "Nagpur";
+            return View(student);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FisrtMVCProject/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace FisrtMVCProject.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        
+    }
+}
